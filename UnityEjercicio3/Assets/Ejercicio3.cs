b35fec5850cdd13a0d85b6bde893da3a479ccabc
joseantoniomoreno <joseantoniomoreno@ceitartessos.com>
2019-10-25 18:15:16 +0200

    ejercicio 3 operaciones

---
+++ UnityEjercicio3/Assets/Ejercicio3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio3 : MonoBehaviour {
+	public int numeroUno = 10;
+	public int numeroDos = 20;
+	public int numeroTres;
+	public float numeroCuatro;
+	public float numeroCinco = 5.5f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("La suma de " + numeroUno + " mas " + numeroTres + " mas " + numeroCuatro + " es igual a = " + (numeroUno + numeroTres + numeroCuatro));
+		Debug.Log("La resta de " + numeroUno + " menos " + numeroDos + " menos " + numeroCinco + " es igual a = " + (numeroUno - numeroDos - numeroCinco));
+		Debug.Log("La operacion de " + numeroUno + " mas " + numeroDos + " por " + numeroCinco + " es igual a = " + ((numeroUno + numeroDos) * numeroCinco));
+		Debug.Log("La operacion de " + numeroUno + " menos " + numeroTres + " dividido entre " + numeroCinco + " mas " + numeroCuatro + " es igual a = " + ((numeroUno - numeroTres) / (numeroCinco + numeroCuatro)));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
